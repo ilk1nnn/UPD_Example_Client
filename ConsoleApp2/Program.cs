843637b6d608bfeeb47c4048de45cef3aad36781
Sule_jr92 <Sule_jr92@STHQ0122-04>
2023-01-17 19:37:49 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var socket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+            var ip = IPAddress.Loopback;
+
+
+            var port = 27001;
+
+
+            EndPoint endpoint = new IPEndPoint(ip, port);
+
+
+            try
+            {
+                while (true)
+                {
+                    var text = Console.ReadLine();
+                    var bytes = Encoding.UTF8.GetBytes(text);
+                    socket.SendTo(bytes, endpoint);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("can not connect to server");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
